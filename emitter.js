7909bbdb6153400a6115de97b8fc2fa92df0c92a
AhmedElsayes <ahmed.elsayes@picosun.com>
2022-06-17 01:14:53 +0300

    eventemitters as a base for chat apps

--- emitter.js
+++ emitter.js
@@ -1,11 +1,33 @@
 // const events = require('events');
 import events from "events"
 
-const caller = new events.EventEmitter();
-// Listen ***
-caller.on("somecall", (msg) => {
-  console.log(msg)
-})
+class Person extends events.EventEmitter {
+  constructor(name, age) {
+    super()
+    this.name = name;
+    this.age = age;
+  }
+}
 
-// Talk ****
-caller.emit("somecall", "Developer should keep practicing")
+const Ahmed = new Person("Ahmed", 25)
+const Ali = new Person("Ali", 24)
+const Hassan = new Person("Hassan", 23)
+const People = [Ahmed, Ali, Hassan]
+
+// listen**
+People.forEach((_person) => {
+  _person.on("SomeCall", (msg) => {
+    console.log(`${msg}: Hi this is me ${_person.name} and my age is ${_person.age}.`)
+  })
+})
+// speak***
+Ahmed.emit("SomeCall", "Call from Ahmed")
+
+// const caller = new events.EventEmitter();
+// // Listen ***
+// caller.on("somecall", (msg) => {
+//   console.log(msg)
+// })
+
+// // Talk ****
+// caller.emit("somecall", "Developer should keep practicing")
