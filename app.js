d7c84e0210297f4b9c46d0c7e011913058f01cde
AhmedElsayes <ahmed.elsayes@picosun.com>
2022-06-16 22:00:17 +0300

    global objects in nodejs

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello Developers")
+// console.log("Hello Developers")
+
+setTimeout(() => {
+  console.log("hello, I am calling after 3 seconds")
+}, 3000);
+
+let time = 0
+const timer = setInterval(() => {
+  time += 1
+  // console.log("Hello, I repeat myself every 2 seconds")
+  console.log(`Hello, I made the call after ${time} seconds`)
+
+  if (time > 20) clearInterval(timer)
+}, 1000);
