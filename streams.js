288cf44617ef8e75f8afdcdeb69894f210d2675e
AhmedElsayes <ahmed.elsayes@picosun.com>
2022-06-18 13:14:40 +0300

    read and write streams in nodejs

--- streams.js
+++ streams.js
@@ -1,8 +1,18 @@
 import fs from 'fs'
 
+// First Lesson *************
+// const readableStream = fs.createReadStream('./sampledata/index.txt', 'utf8')
+
+// readableStream.on('data', (chunk) => {
+//   console.log("**********new chunk is received!**********", '\n')
+//   console.log('\n', chunk)
+// })
+
+// Second lesson **************
 const readableStream = fs.createReadStream('./sampledata/index.txt', 'utf8')
+const writableStream = fs.createWriteStream('./sampledata/writeme.txt')
 
 readableStream.on('data', (chunk) => {
-  console.log("**********new chunk is received!**********", '\n')
-  console.log('\n', chunk)
+  const addedline = '\n' + "**********Separating line**********" + '\n'
+  writableStream.write(chunk+addedline)
 })
